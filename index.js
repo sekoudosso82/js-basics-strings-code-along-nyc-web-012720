3aa01a950174c7dbdb166dc312a97321cf99725d
sekoudosso82 <sekou.dosso82@gmail.com>
2020-03-08 22:19:39 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+let currentUser = 'Grace Hopper'
+const welcomeMessage = "Welcome to Flatbook, "+currentUser +'!' 
+// currentUser = 'GRACE HOPPER'
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser.toUpperCase() +'!' 
+let shortGreeting = "Welcome, " + currentUser.charAt(0) + '!'
